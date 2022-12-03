70e94c43537f3dd46166020e1eefa5e7ff9ebb30
Denz4323 <dsaputr847@gmail.com>
2022-12-03 20:38:25 +0700

    yamete

---
+++ plugins/_floclive1.js
@@ -0,0 +1,31 @@
+import fetch from "node-fetch"
+import { generateWAMessageFromContent } from "@adiwajshing/baileys"
+
+let handler  = async (m, { conn }) => {
+
+ let pp = 'https://i.ibb.co/CJPs6pH/20221203-095917.jpg'
+  try {
+    pp = await conn.profilePictureUrl(m.sender, 'image')
+  } catch (e) {
+  }
+
+let msg = await generateWAMessageFromContent(m.chat, { locationMessage: {
+  degreesLatitude: 0,
+  degreesLongitude: 0,
+  name: nameown,
+  address: wm,
+  url: syt,
+  isLive: true,
+  accuracyInMeters: 0,
+  speedInMps: 0,
+  degreesClockwiseFromMagneticNorth: 2,
+  comment: '',
+  jpegThumbnail: await( await fetch(pp)).buffer()
+}}, { quoted: m })
+
+return conn.relayMessage(m.chat, msg.message, {})
+}
+
+handler.command = /^loc1$/
+handler.owner = false
+export default handler
---
+++ plugins/owner-tesbot.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch'
+import fs from 'fs';
+import crypto from 'crypto'
+let handler = async (m, { conn, text, usedPrefix }) => {
+    const ultah = new Date('September 11 2023 23:59:59')
+    const sekarat = new Date().getTime() 
+    const Kurang = ultah - sekarat
+    const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
+    const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
+    const detek = Math.floor( Kurang % (1000 * 60) / 1000)
+    let sewa = `*🎏 Bot Aktif Kak*
+
+${wm}
+${botdate}
+`
+        let img1 = fs.readFileSync('./thumbnail.jpg');
+        let img2 = fs.readFileSync('./media/bawah.png');
+    return conn.sendButton(m.chat, hiasan, sewa, await (await fetch(`https://i.ibb.co/CJPs6pH/20221203-095917.jpg`)).buffer(), [["🗳️MENU", usedPrefix + "menu"], ["🔭DONASI", usedPrefix + "donasi"]], m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: '',
+                        mediaType: 2,
+                        description: 'anu',
+                        title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        body: `Subscribe`,                                       previewType: 0,
+                        thumbnail: await (await fetch(`https://i.ibb.co/CJPs6pH/20221203-095917.jpg`)).buffer(),
+                        sourceUrl: `${syt}`
+            }
+        }
+    })
+}
+handler.help = ['tesbot']
+handler.tags = ['owner']
+handler.command = ['tesbot']
+handler.owner = true
+export default handler
